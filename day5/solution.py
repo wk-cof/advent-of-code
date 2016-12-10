b4e460c7bc5f32cb7b4b1e0bbbfc3f4e94f40b42
Mikhail Gorbachev <Mikhail Gorbachev>
2016-12-09 20:10:29 -0500

    solution 5 works

--- day5/solution.py
+++ day5/solution.py
@@ -1,18 +1,21 @@
 import hashlib
 m = hashlib.md5()
-#myStr = 'abbhdwsy6082117'
-myStr = 'abbhdwsy5708769'
+
+myStr = 'abbhdwsy'
 counter = 0
 m.update(myStr.encode('utf-8'))
-md5 = m.digest()
-print(md5)
+md5 = m.hexdigest()
 outerCounter = 0
 
 
-# while (not(md5[0] == 0x00 and md5[1] == 0x00 and md5[2] < 0x10 and outerCounter > 7) ):
-# 	m = hashlib.md5()
-# 
-
-	# print(counter)
+while (not(md5[0] == '0' and md5[1] == '0'and md5[2] == '0' and md5[3] == '0' and md5[4] == '0' and outerCounter > 7) ):
+	m = hashlib.md5()
+	myStr = 'abbhdwsy' + str(counter)
+	m.update(myStr.encode('utf-8'))
+	md5 = m.hexdigest()
+	if (md5[0] == '0' and md5[1] == '0'and md5[2] == '0' and md5[3] == '0' and md5[4] == '0'):
+		print(md5)
+		outerCounter += 1
+	counter += 1
 
 print ('gotcha')
